42318ec0a2d8ba3851177ccfcd2bc42e2568f329
venbrinoDev <venbrinosoftwaredeveloper@gmail.com>
2024-03-06 19:12:29 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/skill_harvest_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.skill_harvest_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SkillHarvestApp());
+}
+
+class SkillHarvestApp extends StatelessWidget {
+  const SkillHarvestApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.blue,
+            primary: Colors.white,
+            secondary: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Skill Harvest App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int counter = 1;
+
+  void youJustHitTheButton() {
+    counter = counter + 1;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        title: Text(widget.title, style: const TextStyle(color: Colors.white)),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$counter Numbers',
+              style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          youJustHitTheButton();
+        },
+        tooltip: 'Increment',
+        backgroundColor: Colors.red,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
